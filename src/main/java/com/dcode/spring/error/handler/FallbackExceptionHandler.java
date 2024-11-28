7df933165e6a63f7682b8a5853a354e50a693149
Dinesh R N <rndinesh751@gmail.com>
2024-11-28 13:29:51 +0530

    update fallback handler logic

--- src/main/java/com/dcode/spring/error/handler/FallbackExceptionHandler.java
+++ src/main/java/com/dcode/spring/error/handler/FallbackExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.dcode.spring.error.handler;
 
+import com.dcode.spring.error.utils.Constants;
 import com.dcode.spring.error.utils.HttpStatusMapper;
+import com.dcode.spring.error.utils.ProblemDetailBuilder;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -8,9 +10,9 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.ErrorResponse;
 
 import java.util.Locale;
+import java.util.Optional;
 import java.util.function.Function;
 
 @Component
@@ -32,29 +34,37 @@ public class FallbackExceptionHandler implements ApiExceptionHandler, MessageSou
     @Override
     public Function<Throwable, ProblemDetail> getHandler() {
         return ex -> {
+
             HttpStatusCode httpStatus = httpStatusMapper.getHttpStatus(ex.getClass());
-            String defaultDetail = "";
+            String exceptionMessage = ex.getMessage();
 
-            ErrorResponse.Builder builder = ErrorResponse.builder(ex, httpStatus, defaultDetail);
-            ErrorResponse response = builder.build();
+            ProblemDetailBuilder builder = new ProblemDetailBuilder();
+            ProblemDetail pb = builder.status(httpStatus).detail(exceptionMessage).build();
 
+            updateProblemDetail(ex, pb, messageSource, LocaleContextHolder.getLocale());
 
-
-            ProblemDetail pb = response.getBody();
-            pb.setTitle("Handled by Fallback Handler");
+            Optional.ofNullable(pb.getProperties())
+                    .ifPresentOrElse(props -> props.computeIfAbsent(Constants.ATTRIBUTE_MESSAGE, k -> exceptionMessage),
+                            () -> pb.setProperty(Constants.ATTRIBUTE_MESSAGE, exceptionMessage));
 
             return pb;
         };
     }
 
-    private void updateProblemDetail(Throwable exception, ProblemDetail problemDetail, @Nullable MessageSource messageSource, Locale locale) {
+    private void updateProblemDetail(Throwable exception, ProblemDetail problemDetail,
+            @Nullable MessageSource messageSource, Locale locale) {
+
         if (messageSource != null) {
             Object[] arguments = this.getDetailMessageArguments(messageSource, locale);
-            String detail = messageSource.getMessage(this.getDetailMessageCode(exception), arguments, (String)null, locale);
+            String detail = messageSource.getMessage(this.getDetailMessageCode(exception), arguments, (String) null,
+                    locale);
+
             if (detail != null) {
-                problemDetail.setDetail(detail);
+                problemDetail.setProperty(Constants.ATTRIBUTE_MESSAGE, detail);
             }
-            String title = messageSource.getMessage(this.getTitleMessageCode(), (Object[])null, (String)null, locale);
+
+            String title = messageSource.getMessage(this.getTitleMessageCode(), (Object[]) null, (String) null, locale);
+
             if (title != null) {
                 problemDetail.setTitle(title);
             }
